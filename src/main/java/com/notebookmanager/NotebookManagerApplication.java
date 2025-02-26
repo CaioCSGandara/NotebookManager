c83b711af3b1c4c64cc7bd5dd74d3e1ad4b1f748
CaioCSGandara <caiocsgandara@gmail.com>
2025-02-26 20:14:12 -0300

    first commit

---
+++ src/main/java/com/notebookmanager/NotebookManagerApplication.java
@@ -0,0 +1,13 @@
+package com.notebookmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notebookmanager/NotebookManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notebookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
