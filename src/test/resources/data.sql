fc3a681853b77bd4a39176ea2da124f827bec20a
CaioCSGandara <caiocsgandara@gmail.com>
2025-03-27 21:52:35 -0300

    alterando schema de aluno: retirando campos de data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO aluno (nome, ra, email, telefone, curso, ultimo_login, atualizado_em)
+INSERT INTO aluno (nome, ra, email, telefone, curso)
 VALUES
-    ('Julio Correa', '09135616', 'jcorrea@puccampinas.edu.br', '(19)90914-3014', 'MEDICINA', '2012-11-10T21:12:37', '2012-11-10T21:12:37'),
-    ('Maria Ferreira', '03781923', 'maria.ferreira@puccampinas.edu.br', '(19)90814-2314', 'TERAPIA_OCUPACIONAL', '2021-08-12T21:21:45', '2021-08-12T21:21:45'),
-    ('Fernando Pontes', '90174823', 'fernandohpontes@puccampinas.edu.br', '(19)83914-0945', 'BIOMEDICINA', '2013-01-10T21:12:37', '2013-01-10T21:12:37');
+    ('Julio Correa', '09135616', 'jcorrea@puccampinas.edu.br', '(19)90914-3014', 'MEDICINA'),
+    ('Maria Ferreira', '03781923', 'maria.ferreira@puccampinas.edu.br', '(19)90814-2314', 'TERAPIA_OCUPACIONAL'),
+    ('Fernando Pontes', '90174823', 'fernandohpontes@puccampinas.edu.br', '(19)83914-0945', 'BIOMEDICINA');
 
 INSERT INTO notebook (modelo, patrimonio, status, qtd_emprestimos, atualizado_em)
 VALUES
